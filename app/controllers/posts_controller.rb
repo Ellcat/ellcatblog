086084ef1e910bcf7dc354cbec73f63b9441817c
vagrant <vagrant@precise32.(none)>
2015-08-04 02:04:06 +0000

    got edit, show pages working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.find(params[:id])
   end
 
   # GET /posts/new
@@ -21,40 +22,19 @@ class PostsController < ApplicationController
 
   # GET /posts/1/edit
   def edit
+     @post = Post.find(params[:id])
   end
 
-  # POST /posts
-  # POST /posts.json
+  
   def create
     @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @post }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
+ 
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
-  # DELETE /posts/1
-  # DELETE /posts/1.json
+  
   def destroy
     @post.destroy
     respond_to do |format|
@@ -64,21 +44,10 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:title, :body)
     end
+  
 end
 
-private
-  def authenticate
-    authenticate_or_request_with_http_basic do |name, password|
-      name == "admin" && password == "archer"
-    end
-  end
- 
+
