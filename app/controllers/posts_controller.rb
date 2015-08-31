ff12eb60020fed188f8dc6e7448d7a3f5154ee2f
vagrant <vagrant@precise32.(none)>
2015-08-31 16:20:43 +0000

    change in color

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
  # before_filter :except => [ :create, :index, :show, :edit, :update, :destroy ]
-before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+# before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
   #before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
