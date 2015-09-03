34c1af787d3c95f8b339ba449c93f42324ecf138
vagrant <vagrant@precise32.(none)>
2015-09-03 23:30:42 +0000

    installed devise and basic configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Ellcatblog::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'ellcatblog.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Ellcatblog::Application.routes.draw do
   
+  devise_for :users
   resources :posts
 
   root 'posts#index'
